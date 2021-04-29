06258b307556f256940ebe26ad874dbbbaf9aae0
Magomed Shamaev <ms77grz@gmail.com>
2021-04-29 09:03:45 +0300

    add remove packages

--- app.js
+++ app.js
@@ -18,3 +18,5 @@ const _ = require('lodash');
 const items = [1, [2, [3, [4]]]];
 const flattenItems = _.flattenDeep(items);
 console.log(flattenItems); // [ 1, 2, 3, 4 ]
+
+console.log('Hello, there!');
